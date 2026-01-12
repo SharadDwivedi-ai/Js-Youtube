7b84870274d674cf3c337989731c132314987ee4
SharadDwivedi-ai <srd28nov21@gmail.com>
2026-01-12 04:53:08 +0000

    conversion

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+// let isLoggedIn = 1
+// let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedIn);
+
+// let isLoggedIn = ""
+// let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedIn);
+
+// let isLoggedIn = "Sharad"
+// let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Sharad"=>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
