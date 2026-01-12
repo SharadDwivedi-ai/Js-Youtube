b8370b31f01f0250fe70182db3a60978913dbefb
SharadDwivedi-ai <srd28nov21@gmail.com>
2026-01-12 05:36:00 +0000

    Conversion

---
+++ 01_Basic/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); 
+console.log(null == 0);
+console.log(null >= 0); //Thats why null >= 0 is true and null > 0 is false.
+// The reason is that an equality check == and comparisons > < >= <= work differently
+// Comparisions convert null to a number, treating it as 0. 
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// ===
+console.log("2" === 2);
+
+
+
+
+
+
+
+
