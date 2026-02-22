2074f99089073eb01b9607696bae50fbeee9541e
Sharad Dwivedi <srd28nov21@gmail.com>
2026-02-22 05:31:12 +0000

    Data type summer with Memory(Stack & Heap)

--- 01_Basic/05_datatypesSummery.js
+++ 01_Basic/05_datatypesSummery.js
@@ -2,17 +2,17 @@
 // #Primitive Types (Call by value)
 // String, Number, Boolearn, null, undefined, BigInt, symbol
 
-const score = 100
-const scoreValue = 100.3
-const isLoggedIn = false
-const outsideTemp = null
-console.log(outsideTemp);
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// console.log(outsideTemp);
 
-let userEmail; 
+// let userEmail; 
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id == anotherId);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id == anotherId);
 
 
 // #Non-Primitive (Reference type)
@@ -28,4 +28,30 @@ const myFunction = function(){
 } 
 
 console.log(typeof myFunction)
+
+
+// +++++++++++++++++++++++++++++++Stack & Heap Memory+++++++++++++++++++++++++++++++++++
+
+// Stack (used in Primitive type data) , Heap (used in Non-Primitive type data)
+
+// Stack memory working structure
+// let myYoutubeName = "sharaddwivedi-ai";
+// let anothername = myYoutubeName
+// anothername = "dwivedi-ritz@ai"
+// console.log(anothername);
+// console.log(myYoutubeName);
+
+
+// Heap memory working structure
+let userOne = {
+    userId : 149,
+    userEmail : "sharad.ritz@gmail.com",
+    upi : "srd@ybl"
+}
+let userTwo = userOne
+userTwo.userEmail = "standalone@google.com"
+
+console.log(userOne.userEmail);
+console.log(userTwo.userEmail);
+
 
