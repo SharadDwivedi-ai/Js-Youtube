bc593501a7437a2720eb104abfbb4bb06cab02d3
Sharad Dwivedi <srd28nov21@gmail.com>
2026-02-22 13:45:25 +0000

    String & String Manipulation

---
+++ 01_Basic/06_string.js
@@ -0,0 +1,36 @@
+// const name = "Sharad"
+// const repoCount = 2
+
+// console.log(name + repoCount + "Practice");
+
+// console.log(`Hello ! My name is ${name}, My Repo count is ${repoCount}`);
+
+// const gameName = new String('Sharad')
+
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('d'));
+
+const gameName = new String('Sharad-SR')
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5, 3)
+// console.log(anotherString);
+
+// const newStringOne = "  Sharad   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://sharad.com/sharad%20dwivedi" //%20 is converted by browser of space
+console.log(url.replace('%20', '-'))
+console.log(url.includes('ritz'))
+
+
+console.log(gameName.split('-'));
+
+
