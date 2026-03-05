6fc670feb113e353515a6f20d2ab3b82d9d09025
Sharad Dwivedi <srd28nov21@gmail.com>
2026-03-05 12:41:59 +0000

    IIFE

--- 03_Basic/04_iife.js
+++ 03_Basic/04_iife.js
@@ -1,5 +1,14 @@
-// Immediately Invoked Function Expressions (IIFE)
-function chai(){
+// Immediately Invoked Function Expressions (IIFE) : Global variable makes pollution on scope then protect from this pollution using the IIFE.  
+(function chai(){
+    // Name IIFE
     console.log('DB Connected');
-}
-chai()
+})();               // (;) required to stop the IIFE
+
+(()=>{
+    //Unnamed IIFE
+console.log('DB Connected Two');
+})();
+
+((name)=>{
+console.log(`DB Connected Two ${name}`);
+})('Sharad')
