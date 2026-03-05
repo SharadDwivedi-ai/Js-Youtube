62c17c3ab017ce830b34377b6cc270a74712c658
Sharad Dwivedi <srd28nov21@gmail.com>
2026-03-05 10:12:44 +0000

    Fundamental

---
+++ 03_Basic/02_scope.js
@@ -0,0 +1,14 @@
+// var a=300
+
+let a= 13
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Inner value of a : ", a);
+    
+}
+
+// console.log(a); //Block scope
+// console.log(b); //Block scope 
+console.log(a); //global scope
