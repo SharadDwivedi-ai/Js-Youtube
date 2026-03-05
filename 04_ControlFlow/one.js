6b3f3e30be6f957d5b8cbbd4a760ab0b12cdc8a4
Sharad Dwivedi <srd28nov21@gmail.com>
2026-03-05 14:45:30 +0000

    If else condition

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,41 @@
+// if condition 
+// <,>,<=,>=,==,!=, ===
+// if (true){
+//   condition true then code execute 
+// }
+// if (false){
+//  condition false then code not execute 
+// }
+
+// const isUserLoggedin = true
+// if(2 ==="2"){
+//     console.log("Excuted");
+// }else{
+//     console.log("Not executed");
+// }
+
+// const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+// }
+// console.log(`User Power: ${power}`);
+
+// const balance = 1000
+// if(balance > 500) console.log("test");
+// if(balance > 500) console.log("test"), console.log("test2"); Not follow this type of coding
+
+const userLoggedIn = true
+const debitCard = true 
+const loggedFromGoogle = false
+const loggedFromEmail = true
+if(userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+}
+
+if(loggedFromGoogle || loggedFromEmail){
+    console.log("User Logged In");    
+}
+ 
+
+
