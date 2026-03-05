cab24fe97c8376670c94ab0ed622b647ec200c82
Sharad Dwivedi <srd28nov21@gmail.com>
2026-03-05 15:28:01 +0000

    Truthly falsy and Nullish coalescing operator

---
+++ 04_ControlFlow/truthy.js
@@ -0,0 +1,37 @@
+// const userEmail = "R@sharad.ai"    
+// if(userEmail){
+//     console.log("Got User email");    
+// } else {
+//     console.log("Don't have user email");
+// }
+
+// Falsy values: false, 0, -0, Bigint(0n),"", null, undefined, NaN
+// Truthly values: "0", 'false', " " string with space, [], {}, function(){}
+
+// check empty array 
+// const userEmail = []
+// if (userEmail.length === 0){
+//     console.log("Array is empty")
+// }
+
+// check empty object 
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0){
+//   console.log("Object is empty ");
+// }
+
+// Nullish coalescing operator (??) : null, undefined
+
+// let val1;
+// // val1 = 5 ?? 10  //5
+// // val1 = null ?? 10  //10 
+// // val1 = undefined ?? 15  //15
+// val1 = null ?? 10 ?? 20
+// console.log(val1);
+
+// Terniary operator 
+// Syntex : condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice>80 ? console.log("More than 80") : console.log("Less than 80")
+
+
